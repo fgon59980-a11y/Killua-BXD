b066e8f1258db4c08daf507bddeeb42335d4c726
fgon59980-a11y <fgon59980@gmail.com>
2025-09-06 10:40:05 +0100

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,8 +1,8 @@
 
 module.exports = {
-  SESSION_ID: "",  // add your Session Id here
+  SESSION_ID: "malvin~ljdlXDoQ#-wRF-UuNkgXUftZK9QsUiADjjWuqMygTRVkkln8bpyI",  // add your Session Id here
   
-  OWNER_NUMBER: "263714757857", // put your phone number here
+  OWNER_NUMBER: "+23569750414", // put your phone number here
   
   PREFIX: ".", // prefix (e.g., ., /, !, *)
   
